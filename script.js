6eb7a0f9bbe98423cd216e7b1ef7e61e987b8458
jamshidakramovich14 <jamshidturdiyev103>
2024-01-26 23:16:56 +0500

    all files

---
+++ script.js
@@ -0,0 +1,59 @@
+"use strict";
+// 12-DARS /////////////////////////////////////////////
+
+// 1 - masala
+
+// const createHelloWorld = function(){
+//     return "Hello World";
+// }
+
+// console.log(createHelloWorld());
+
+// 2 - masala
+
+// const createCounter = (n) =>{
+//     return n++;
+// }
+
+// console.log(createCounter(6));
+
+// 3 - masala
+
+// let praducts = [
+//     {
+//         name : "Apelsin",
+//         price : 10000,
+
+//     },
+//     {
+//         name : "Olma",
+//         price : 12000,
+//         wight : 1,
+//     },
+//     {
+//         name : "Mandalin",
+//         price : 8000,
+//     },
+//     {
+//         name : "Banan",
+//         price : 20000,
+//     },
+// ];
+// let summa = 0;
+// for(let i = 0; i<praducts.length ; i++ ){
+//     summa += praducts[i].price;
+// }
+// console.log(summa);
+
+// 5- misol
+
+//  const obj = {
+//     "it": 20,
+//     "mushuk": 10,
+//     "sigir": 200,
+//     "tovuq": 2
+// };
+
+// const obj2 =[];
+
+// console.log(obj2);
